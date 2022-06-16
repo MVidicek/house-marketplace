50713fb601bb96e5900aa892e15af829f61f8f0f
MVidicek <mvidicek@protonmail.com>
2022-06-16 13:59:49 +0200

    Sign in & sign up forms

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,14 @@
-import { userNavigate, useLocation } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 import { ReactComponent as OfferIcon } from '../assets/svg/localOfferIcon.svg';
 import { ReactComponent as ExploreIcon } from '../assets/svg/exploreIcon.svg';
 import { ReactComponent as PersonOutlineIcon } from '../assets/svg/personOutlineIcon.svg';
 
 function Navbar() {
   const location = useLocation();
-  const navigate = userNavigate();
+  const navigate = useNavigate();
 
   const pathMatchRoute = (route) => {
-    if (route === location.path) {
+    if (route === location.pathname) {
       return true;
     }
   };
