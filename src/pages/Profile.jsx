64a66599a06c6316362d5cd914b1a0cd58c0a009
MVidicek <mvidicek@protonmail.com>
2022-06-16 14:48:48 +0200

    User sign in

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,18 @@
+import { useEffect, useState } from 'react';
+import { getAuth } from 'firebase/auth';
+
 function Profile() {
-  return (
-    <div>
-      <h1>Profile</h1>
-    </div>
-  );
+  const [user, setUser] = useState(null);
+
+  const auth = getAuth();
+
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      setUser(user);
+    });
+  }, [auth]);
+
+  return user ? <h1>{user.displayName}</h1> : <h1>Not Logged In</h1>;
 }
 
 export default Profile;
