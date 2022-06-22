9cd38188f6024c4d98a9702f905e7d3e68fc4fd6
MVidicek <mvidicek@protonmail.com>
2022-06-22 19:18:14 +0200

    Fixed explore page swiper component

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -16,13 +16,13 @@ function Slider() {
   useEffect(() => {
     const fetchListings = async () => {
       const listingsRef = collection(db, 'listings');
-      const q = query(listingsRef, orderBy('timestamp', 'desc'), limit(10));
+      const q = query(listingsRef, orderBy('timestamp', 'desc'), limit(5));
       const querySnap = await getDocs(q);
 
       let listings = [];
 
       querySnap.forEach((doc) => {
-        return listings.push({
+        listings.push({
           id: doc.id,
           data: doc.data(),
         });
@@ -47,24 +47,26 @@ function Slider() {
       <>
         <p className='exploreHeading'>Recommended</p>
 
-        <Swiper slidesPerView={1} pagination={{ clickable: true }}>
-          {listings.map((data, id) => (
+        <Swiper slidesPerView={2} pagination={{ clickable: true }}>
+          {listings.map(({ data, id }) => (
             <SwiperSlide
               key={id}
               onClick={() => navigate(`/category/${data.type}/${id}`)}
             >
-              <div
-                className='swiperSlideDiv'
-                style={{
-                  background: `url(${data.imgUrls[0].toString()}) center no-repeat`,
-                  backgroundSize: 'cover',
-                }}
-              >
-                <p className='swiperSlideText'>{data.name}</p>
-                <p className='swiperSlidePrice'>
-                  ${data.discountedPrice ?? data.regularPrice}{' '}
-                  {data.type === 'rent' && '/ month'}
-                </p>
+              <div class='flex-container'>
+                <div
+                  style={{
+                    background: `url(${data.imgUrls[0]}) center no-repeat`,
+                    backgroundSize: 'cover',
+                  }}
+                  className='swiperSlideDiv content'
+                >
+                  <p className='swiperSlideText'>{data.name}</p>
+                  <p className='swiperSlidePrice'>
+                    ${data.discountedPrice ?? data.regularPrice}{' '}
+                    {data.type === 'rent' && '/ month'}
+                  </p>
+                </div>
               </div>
             </SwiperSlide>
           ))}
