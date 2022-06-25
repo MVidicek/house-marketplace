f5b14811bc563e0bc1bfe839da00b07805e51840
MVidicek <mvidicek@protonmail.com>
2022-06-25 13:35:05 +0200

    Fixed listing contact

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -128,7 +128,7 @@ function Listing() {
           </MapContainer>
         </div>
 
-        {auth.currentUser?.uid === listing.userRef && (
+        {auth.currentUser?.uid !== listing.userRef && (
           <Link
             to={`/contact/${listing.userRef}?listingName=${listing.name}`}
             className='primaryButton'
